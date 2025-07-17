f17f805a65f212b348c3040c98f13d7114545320
israel damilola <ojuaisrael92@gmail.com>
2025-07-17 09:18:58 +0000

    Upload New File

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,14 @@
+<tr>
+<td>
+<table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td class="content-cell" align="center">
+    <p style="font-size: 13px; color: #9CA3AF;">
+        © {{ date('Y') }} EasyStore. All rights reserved.
+    </p>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+
