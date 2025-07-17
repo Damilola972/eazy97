4eefd273e0eaed1b4e81a064cb12ff20fd94c53e
israel damilola <ojuaisrael92@gmail.com>
2025-07-17 07:42:54 +0000

    Upload New File

---
+++ app/Mail/OrderSuccess.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Order;
+
+class OrderSuccess extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $order;
+
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    public function build()
+    {
+        return $this->subject('🛒 Order Confirmed – Tracking ID: ' . $this->order->tracking_id)
+                    ->view('emails.order_success');
+    }
+}
